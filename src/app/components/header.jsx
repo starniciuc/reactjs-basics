1b942a3e63933e1a1fb8243c4a42d689b6914879
Mihai Starniciuc <mihai.starniciuc@endava.com>
2017-06-09 08:58:38 +0300

    Dump component

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 
-export default class Header extends React.Component {
-
-    render() {
-        return (<nav className="navbar navbar-default">
-            <div className="container">
-                <div className="navbar-header">
-                    <ul className="nav navbar-nav">
-                        <li><Link to="/home">Home</Link></li>
-                        <li><Link to="/user/4">User</Link></li>
-                    </ul>
-                </div>
+const Header = (props) => {
+    return (<nav className="navbar navbar-default">
+        <div className="container">
+            <div className="navbar-header">
+                <ul className="nav navbar-nav">
+                    <li><Link to="/home">Home</Link></li>
+                    <li><Link to="/user/4">User</Link></li>
+                </ul>
             </div>
-        </nav>)
-    }
-}
+        </div>
+    </nav>)
+};
+
+export default Header;
--- src/app/components/root.jsx
+++ src/app/components/root.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 import Header from './header'
 
-export default class Root extends React.Component {
-    render() {
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-xs-10 col-xs-offset-1">
-                        <Header/>
-                    </div>
+const Root = (props) => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-xs-10 col-xs-offset-1">
+                    <Header/>
                 </div>
-                <div className="row">
-                    <div className="col-xs-10 col-xs-offset-1">
-                        {this.props.children}
-                    </div>
+            </div>
+            <div className="row">
+                <div className="col-xs-10 col-xs-offset-1">
+                    {props.children}
                 </div>
-            </div>);
-    }
-}
+            </div>
+        </div>);
+};
+export default Root;
